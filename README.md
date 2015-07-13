1c8c30406b5119838e01f09ee8ec382bc087fad7
Ubuntu <meyskens@me.com>
2015-07-13 11:46:13 +0000

    Working version

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # node-fleet-api
-A simple Node.JS module to interact with Fleet on CoreOS
+A simple Node.JS module to interact with Fleet on CoreOS. The module is currently capable to get info on a unit file, starting and stopping them and destroying them.
+
+## Usage
+```
+var fleet=require("node-fleet-api")("http://127.0.0.1:49153") //This is the HTTP endpoint with the http:// bit without the / on the and
+fleet.startUnit("test.service",function(err,res){});
+
+```
+
+## Functions
+
+### Create a unit file
+```
+fleet.newUnit("name of unit",{unit content in JS object},callback)
+```
+for thr format of the JS object see [the official documentation](https://github.com/coreos/fleet/blob/master/Documentation/api-v1.md)
+
+### Get a unit file
+```
+fleet.getUnit("name of unit",callback)
+```
+
+### Start a unit file
+```
+fleet.startUnit("name of unit",callback)
+```
+### Stop a unit file
+```
+fleet.stpoUnit("name of unit",callback)
+```
+### Destroy a unit file
+```
+fleet.destroyUnit("name of unit",callback)
+```
--- index.js
+++ index.js
@@ -1,41 +1,92 @@
-var etcdDiscover=require("./etcd/discover.js")
+var rest = require("restler")
+module.exports = function(host) {
+	var fleetexport = {}
 
-var fleet=function(host) {
-	var fleetexport ={}
-	
-	fleetexport.newUnitFile=function (name,data,callback) {
-		
+	fleetexport.newUnit = function(name, data, callback) {
+		rest.put(host + "/fleet/v1/units/" + name, {
+			data: JSON.stringify(data),
+			headers: {
+				"Content-Type": "application/json"
+			},
+			timeout: 100000
+		}).on("complete", function(data, info) {
+			if (typeof data.error !== "undefined") {
+				callback(data)
+				return
+			}
+			callback(null, data)
+		}).on("timeout", function() {
+			callback("timeout")
+		})
 	}
-	
-	fleetexport.startUnit=function (unit,callback) {
-		
+
+	fleetexport.startUnit = function(unit, callback) {
+		rest.put(host + "/fleet/v1/units/" + unit, {
+			data: JSON.stringify({
+				desiredState: "launched"
+			}),
+			headers: {
+				"Content-Type": "application/json"
+			},
+			timeout: 100000
+		}).on("complete", function(data, info) {
+			if (typeof data.error !== "undefined") {
+				callback(data)
+				return
+			}
+			callback(null, data)
+		}).on("timeout", function() {
+			callback("timeout")
+		})
 	}
-	
-	fleetexport.stopUnit=function (unit,callback) {
-		
+
+	fleetexport.stopUnit = function(unit, callback) {
+		rest.put(host + "/fleet/v1/units/" + unit, {
+			data: JSON.stringify({
+				desiredState: "inactive"
+			}),
+			headers: {
+				"Content-Type": "application/json"
+			},
+			timeout: 100000
+		}).on("complete", function(data, info) {
+			if (typeof data.error !== "undefined") {
+				callback(data)
+				return
+			}
+			callback(null, data)
+		}).on("timeout", function() {
+			callback("timeout")
+		})
 	}
-	
-	fleetexport.destroyUnit=function (unit,callback) {
-		
+
+	fleetexport.destroyUnit = function(unit, callback) {
+		rest.delete(host + "/fleet/v1/units/" + unit, {
+			timeout: 100000
+		}).on("complete", function(data, info) {
+			if (typeof data.error !== "undefined") {
+				callback(data)
+				return
+			}
+			callback(null, data)
+		}).on("timeout", function() {
+			callback("timeout")
+		})
 	}
-	
-	fleetexport.getUnit=function(unit,callback) {
-		
+
+	fleetexport.getUnit = function(unit, callback) {
+		rest.get(host + "/fleet/v1/units/" + unit, {
+			timeout: 100000
+		}).on("complete", function(data, info) {
+			if (typeof data.error !== "undefined") {
+				callback(data)
+				return
+			}
+			callback(null, data)
+		}).on("timeout", function() {
+			callback("timeout")
+		})
 	}
-	
+
 	return fleetexport;
 }
-
-
-
-
-
-module.exports.connectViaEtcdDiscover=function (discover) {
-	var hosts=etcdDiscover.getHosts(discover)
-	return fleet("");
-}
-
-module.exports.connectDirect=function (host) {
-	return fleet(host)
-}
-
